f4158cdbba112b0d44e198de9ec22ddb5f577b96
Jordi Orlando <D1SC0tech@users.noreply.github.com>
2016-01-30 02:47:42 -0600

    Dedupe code and allow for multiple queries
    Also mitigated SQL injection.
    Warning: completely untested.

--- controllers/api/search.js
+++ controllers/api/search.js
@@ -5,113 +5,80 @@ var router = require('express').Router();
 var pg = require('pg');
 var connect = require('../../auth');
 var winston = require('../../logger');
-var table = require('../../table');
+
+function Package(id, name, salary) {
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+
+    return this;
+}
 
 
 router.get('/', function (req, res, next) {
     res.send('Nothing here, move along');
 });
 
-function Package(id,name,salary){
-    this.id = id;
-    this.name = name;
-    this.salary = salary;
-    return this;
-}
-
-
 router.post('/', function (req, res, next) {
-    // If they searched by name, run this
-    if(req.body.name){
-        var query;
+    var text = "SELECT DISTINCT personid, name, totalSal FROM " + require('../../table');
+    var conditions = [];
+    var values = [];
+    
+    var simpleObjs = [];
+
+    if (req.body.name) {
         var names = req.body.name.split(' ');
-        var client = new pg.Client(connect);
-        var simpleObjs = [];
-        var name;
+        var name = names[0] + "%";
+        name = names.length === 1 ? "%" + name : name + names[names.length - 1];
 
-        if(names.length === 1){
-            name = "%" + names[0] + "%";
-            name = name.replace("'","''");
-        } else {
-            name = names[0]+"%"+names[names.length - 1];
-            name = name.replace("'","''");
+        // Don't de-dupe if a name is specified
+        text = text.replace(" DISTINCT", "");
+
+        conditions.push('name LIKE');
+        values.push(name.replace("'", "''"));
+
+        winston.info({query: name, address: req.connection.remoteAddress});
+    }
+    if (req.body.dept) {
+        conditions.push('department =');
+        values.push(' ' + req.body.dept);
+
+        // winston.info(dept);
+    }
+    if (req.body.campus) {
+        conditions.push('campus =');
+        values.push(' ' + req.body.campus);
+    }
+    
+    for (var index in conditions) {
+        text += [
+            ' ' + index === 0 ? 'WHERE' : 'AND',
+            conditions[index],
+            '$' + (index + 1) // Should this be in the form $1 or ($1) or $1::string
+        ].join(' ');
+    }
+
+    pg.connect(connect, function(err, client, done) {
+        if (err) {
+            return winston.error('could not connect to postgres', {error: err});
         }
-
-        winston.info({ query: name.replace("''","'"), address: req.connection.remoteAddress });
-
-        query = client.query("SELECT personid, name, totalSal FROM " + table + " WHERE name LIKE '"+name+"'" );
-        client.connect(function(err) {
-            if(err) {
-                winston.error('could not connect to postgres', { error: err });
-                return
+        var query = client.query(text, values, function(err, result) {
+            done();
+            
+            if (err) {
+                return winston.error('error running query', {error: err});
             }
-            query.on('row',function(row,result) {
-                var person = new Package(row.personid,row.name,row.totalsal);
-                simpleObjs.push(person);
-                result.addRow(row);
-
-            });
-            query.on('error',function(err){
-                winston.error({ error: err });
-                return;
-            })
-            query.on('end',function(result){
-                res.send(simpleObjs);
-                client.end();
-            })
         });
-    }
-    // Triggered by department search
-    if(req.body.dept){
-        var query;
-        var dept = req.body.dept;
-        client = new pg.Client(connect);
-        var simpleObjs = [];
-        winston.info(dept);
-
-        query = client.query("SELECT distinct personid, name, totalSal From "+table+" WHERE department = '"+dept+"'");
-        client.connect(function(err){
-            if(err) {
-                winston.error(err);
-                return;
-            }
-            query.on('row', function(row,result){
-                var person = new Package(row.personid,row.name,row.totalsal);
-                simpleObjs.push(person);
-                result.addRow(row);
-            });
-            query.on('end',function(result){
-                res.send(simpleObjs);
-                client.end()
-            })
-        })
-
-    }
-    // Triggered by campus search
-    if(req.body.campus){
-        var query;
-        var campus = req.body.campus;
-        client = new pg.Client(connect);
-        var simpleObjs = [];
-        query = client.query("SELECT distinct personid, name, totalSal FROM "+table+" WHERE campus = ' "+campus+"'"); // PLEASE NOTE THE SPACE BEFORE THE CAMPUS VARIABLE - SOMETHING ISN'T RIGHT IN THE CLEANING SCRIPT
-        client.connect(function(err){
-            if(err) {
-                winston.error(err);
-                return;
-            }
-            query.on('row', function(row,result){
-                var person = new Package(row.personid,row.name,row.totalsal);
-                simpleObjs.push(person);
-                result.addRow(row);
-            });
-            query.on('end',function(result){
-                res.send(simpleObjs);
-                client.end()
-            })
-        })
-
-    }
-
+        query.on('row', function(row, result) {
+            var person = new Package(row.personid, row.name, row.totalsal);
+            simpleObjs.push(person);
+            result.addRow(row);
+        });
+        query.on('end', function(result) {
+            res.send(simpleObjs);
+            client.end();
+        });
+    });
 });
 
 module.exports = router;
