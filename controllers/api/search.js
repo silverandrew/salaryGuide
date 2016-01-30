8ae9f8e3c25cdde3650f712a85453073372d835b
Eli Murray <emurray@tampabay.com>
2016-01-30 12:08:35 -0500

    cleaned up pr a little bit. Removed sorting for now until we have a way to test it in the UI. ng/searchSVC.js needs some love now

--- controllers/api/search.js
+++ controllers/api/search.js
@@ -20,59 +20,54 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-    var text = "SELECT DISTINCT personid, name, totalSal FROM " + require('../../table');
+    var text = "SELECT DISTINCT personid, name, totalSal FROM " + require('../../table') + " WHERE ";
     var conditions = [];
     var values = [];
+    var paramNum = 0;
     
     var simpleObjs = [];
 
     if (req.body.name) {
-        var names = req.body.name.split(' ');
-        var name = names[0] + "%";
-        name = names.length === 1 ? "%" + name : name + names[names.length - 1];
+
+        var name = "%" + req.body.name.split(' ').join("%") + "%";
 
         // Don't de-dupe if a name is specified
         text = text.replace(" DISTINCT", "");
 
-        conditions.push('name LIKE');
-        values.push(name.replace("'", "''"));
+        conditions.push('name ILIKE $' + ++paramNum);
+        values.push(name);
 
         winston.info({query: name, address: req.connection.remoteAddress});
     }
     if (req.body.dept) {
-        conditions.push('department =');
-        values.push(' ' + req.body.dept);
+        conditions.push('department = $' + ++paramNum);
+        values.push(req.body.dept);
 
         // winston.info(dept);
     }
     if (req.body.campus) {
-        conditions.push('campus =');
-        values.push(' ' + req.body.campus);
+        conditions.push('campus = $' + ++paramNum);
+        values.push(" " + req.body.campus);
     }
     
-    for (var index in conditions) {
-        text += [
-            ' ' + index === 0 ? 'WHERE' : 'AND',
-            conditions[index],
-            '$' + (index + 1) // Should this be in the form $1 or ($1) or $1::string
-        ].join(' ');
-    }
+    text += conditions.join(" AND ");
 
-    if (req.body.sort) {
-        values.push(req.body.sort.field);
-        text += " ORDER BY $" + values.length + req.body.sort.desc ? " DESC" : " ASC";
-    }
+    // This should be reviewed further.
+    //if (req.body.sort) {
+    //    values.push(req.body.sort.field);
+    //    text += " ORDER BY $" + values.length + req.body.sort.desc ? " DESC" : " ASC";
+    //}
 
     pg.connect(connect, function(err, client, done) {
         if (err) {
             return winston.error('could not connect to postgres', {error: err});
         }
+        console.log(text, values);
         var query = client.query(text, values, function(err, result) {
-            done();
-            
             if (err) {
                 return winston.error('error running query', {error: err});
             }
+            done();
         });
         query.on('row', function(row, result) {
             var person = new Package(row.personid, row.name, row.totalsal);
